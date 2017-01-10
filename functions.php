ce926bdc792eb020600f1a2fa14b49913d933bf4
David Michael Brattoli <dbrattoli@lorainccc.edu>
2017-01-10 12:38:28 -0500

    updated the add to rest Api function

--- functions.php
+++ functions.php
@@ -374,7 +374,7 @@ function lccc_athletic_cpt_add_taxonomy_filters() {
 add_action( 'restrict_manage_posts', 'lccc_athletic_cpt_add_taxonomy_filters' );
 
 function sb_add_tax_to_api() {
-    $mytax = get_taxonomy( 'athletic_category' );
+    $mytax = get_taxonomy( 'athletic-category' );
     $mytax->show_in_rest = true;
 }
 add_action( 'init', 'sb_add_tax_to_api', 30 );
