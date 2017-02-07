b76c9719b372eea6af8c11b5b130ff9d203aa2e4
Joe Querin <joecue@gmail.com>
2017-02-07 14:27:27 -0500

    Updated Mobile App and Maps and Directions Links

--- footer.php
+++ footer.php
@@ -11,13 +11,13 @@
 
 	<footer id="colophon" class="small-12 medium-12 large-12 columns site-footer" role="contentinfo">
 		  <div class="row text-center medium-text-left">
-    <div class="large-4 medium-4 columns"><a href="http://www.lorainccc.edu/"><img class="footer-logo" src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/lccclogo_white.svg" alt="" width="220" height="42.5"/></a> 
+    <div class="large-4 medium-4 columns"><a href="http://www.lorainccc.edu/"><img class="footer-logo" src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/lccclogo_white.svg" alt="" width="220" height="42.5"/></a>
       <h2>Connect with LCCC</h2>
       <ul class="menu footer-sm-links">
         <li><a href="https://www.facebook.com/LCCCAthletics" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/facebook_white.svg" height="30" width="30" alt="" /></a></li>
         <li><a href="https://twitter.com/lcccathletics " target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/twitter_white.svg" height="30" width="30" alt="" /></a></li>
       </ul>
-      <a href="#" target="_blank" class="clearfix mobile-app-link"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/smartphone_yellow.svg" heigth="33" width="20" alt=""/>
+      <a href="/campus-technology/lccc-mobile/" title="Download LCCC's Mobile App" target="_blank" class="clearfix mobile-app-link"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/smartphone_yellow.svg" heigth="33" width="20" alt=""/>
       <h2>LCCC'S<br />
         Mobile App</h2>
       </a>
@@ -27,7 +27,7 @@
      </p>
      <?php } ?>
      </div>
-					<?php 
+					<?php
 					$footer_contact_email = get_option( 'lccc_footer_email', '' );
 					$footer_contact_phone = get_option( 'lccc_footer_phone', '' );
 					?>
@@ -35,21 +35,21 @@
       <h2>Contact Athletics</h2>
    <p>1005 N Abbe Rd<br>
         Elyria, OH 44035<br>
-     <?php   
+     <?php
 					if($footer_contact_phone != ''){
-									echo $footer_contact_phone.'<br>'; 
-				 }else{ ?>   
+									echo $footer_contact_phone.'<br>';
+				 }else{ ?>
 								1-800-995-LCCC (5222)<br>
 								or (440) 365-5222<br>
-				<?php } 
+				<?php }
 				if($footer_contact_email != ''){?>
-				<a class="underline" href="mailto:<?php echo $footer_contact_email; ?>"><?php echo $footer_contact_email; ?></a> 
-				<?php }else {?>   
-					<a class="underline" href="mailto:info@lorainccc.edu">info@lorainccc.edu</a> 
+				<a class="underline" href="mailto:<?php echo $footer_contact_email; ?>"><?php echo $footer_contact_email; ?></a>
+				<?php }else {?>
+					<a class="underline" href="mailto:info@lorainccc.edu">info@lorainccc.edu</a>
 					<?php } ?>
 								</p>
         <ul class="underline">
-        <li><a href="https://test.lorainccc.edu/about/map-and-directions-to-lccc/">Map and Directions</a></li>
+        <li><a href="/about/map-and-directions-to-lccc/">Map and Directions</a></li>
       </ul>
     </div>
     <div class="large-4 medium-4 columns">
@@ -66,7 +66,7 @@
 		</nav><!-- .main-navigation -->
 	<?php endif; ?>
     </div>
-            
+
   </div>
 </footer><!-- #colophon -->
 </div><!-- #page -->
